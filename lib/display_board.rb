f8b013b9a9b40c824f02e922ec2bec696a6b4997
paorendor rendor <paorendor@gmail.com>
2018-09-16 17:41:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-define display_board 
+define display_board ()
   puts "   |   |   \n"
   puts "-----------\n"
   puts "   |   |   \n"
